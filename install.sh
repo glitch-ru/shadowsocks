51de5af58600d27d5f3967ad1a73cfc63b64163c
Glitch <pulse7151@gmail.com>
2025-10-26 18:33:31 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-apt update && apt upgrade -y && apt install curl && apt install docker.io
-sudo bash -c "$(wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-apps/master/server_manager/install_scripts/install_server.sh)"
+echo "123"
